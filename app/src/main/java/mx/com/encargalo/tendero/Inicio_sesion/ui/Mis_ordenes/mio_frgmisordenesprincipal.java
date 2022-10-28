0a5f10fbcf12652a4454fbb0279c20aae244b934
Lucho1600 <72871399@continental.edu.pe>
2022-10-28 15:39:52 -0500

    segundo commit

--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mis_ordenes/mio_frgmisordenesprincipal.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mis_ordenes/mio_frgmisordenesprincipal.java
@@ -3,20 +3,31 @@ package mx.com.encargalo.tendero.Inicio_sesion.ui.Mis_ordenes;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import mx.com.encargalo.R;
 
 
 public class mio_frgmisordenesprincipal extends Fragment {
+    TextView txtdetalle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mio_frgmisordenesprincipal, container, false);
+        View vista = inflater.inflate(R.layout.fragment_mio_frgmisordenesprincipal, container, false);
+        txtdetalle= vista.findViewById(R.id.mio_moptxtdetalle);
+        txtdetalle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.nav_misordenesdetalleproducto);
+            }
+        });
+        return vista;
     }
 }
