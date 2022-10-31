2813845de350648681a9b3f673a667e3a2ec0045
Davidcv <dcamposvillegas@gmail.com>
2022-10-30 22:21:38 -0500

    Flujo de inicio de sesión agregado

--- app/src/main/java/mx/com/encargalo/activity_is_actcrearunacuenta.java
+++ app/src/main/java/mx/com/encargalo/activity_is_actcrearunacuenta.java
@@ -1,14 +1,25 @@
 package mx.com.encargalo;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_is_actcrearunacuenta extends AppCompatActivity {
-
-    @Override
+Button btncrearcuenta;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_is_actcrearunacuenta);
-    }
+
+        btncrearcuenta=(Button)findViewById(R.id.is_ispbtnRegistrarse);
+        btncrearcuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent loguearfb = new Intent(activity_is_actcrearunacuenta.this, activity_is_actverificacioncodigo.class);
+                startActivity(loguearfb);
+            }
+        });
+
+        }
 }
--- app/src/main/java/mx/com/encargalo/activity_is_actverificacioncodigo.java
+++ app/src/main/java/mx/com/encargalo/activity_is_actverificacioncodigo.java
@@ -1,14 +1,30 @@
 package mx.com.encargalo;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import mx.com.encargalo.tendero.Inicio_sesion.MainActivity;
+import mx.com.encargalo.tendero.Inicio_sesion.ui.Mis_ordenes.mio_frgmisordenesprincipal;
 
 public class activity_is_actverificacioncodigo extends AppCompatActivity {
+    Button btnvalidar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_is_actverificacioncodigo);
+        btnvalidar=(Button)findViewById(R.id.is_vcbtncontinuar);
+        btnvalidar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(android.R.id.content, new mio_frgmisordenesprincipal()).commit();}
+
+        });
+
     }
 }
